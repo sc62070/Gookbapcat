0c618141446ab431b8b32cad6c217300155c0b07
Hyuk Jin Choi <sc62070@gmail.com>
2022-05-03 09:43:39 +0900

    Initial commit

---
+++ Assets/Script/ClickButton.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickButton : MonoBehaviour{
+
+    public int gold = 0;
+
+    public int goldPerClick = 1;
+
+    public void OnClick()
+    {
+        gold = gold + goldPerClick;
+    }
+
+}
